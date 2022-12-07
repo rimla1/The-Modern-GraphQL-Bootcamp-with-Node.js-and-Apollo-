e3e90738be69a81c2776b5ca67d1dacbd3abe557
Almir Muminovic <projectalmir@gmail.com>
2022-12-07 15:30:16 +0100

    Creating first GraphQL API

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,34 @@
-import addTwoNumbers, { substract } from "./math";
+import { GraphQLServer } from "graphql-yoga";
 
-console.log(addTwoNumbers(5, 20));
-console.log(substract(5, 20));
+// Type Definitions (Scema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        age: String!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query";
+    },
+    name() {
+      return "Almir";
+    },
+    age() {
+      return "20";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("Server is up and running on default port 4000");
+});
