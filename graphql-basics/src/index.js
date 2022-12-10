81caf774afefc68817b2288425a4013d98962bd7
Almir Muminovic <projectalmir@gmail.com>
2022-12-10 12:07:19 +0100

    Creating posts query that return array of posts

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,12 +3,10 @@ import { GraphQLServer } from "graphql-yoga";
 // Type Definitions (Scema)
 const typeDefs = `
     type Query {
-        addNNumbers(numbers: [Float!]!): Float!
-        addTwoNumbers(a: Float!, b: Float!): Float!
-        greeting(name: String, position: String): String!
-        grades: [Int!]!
+
         me: User!
         post: Post!
+        posts: [Post!]!
     }
 
     type User {
@@ -29,24 +27,21 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    addNNumbers(parent, args, ctx, info) {
-      let result = 0;
-      for (let i = 0; i < args.numbers.length; i++) {
-        result += args.numbers[i];
-      }
-      return result;
-    },
-    addTwoNumbers(parent, args, ctx, info) {
-      return args.a + args.b;
-    },
-    greeting(parent, args, ctx, info) {
-      if (!args.name && !args.position) {
-        return "Hello!";
-      }
-      return `Hello, ${args.name}! You are my favourite ${args.position}`;
-    },
-    grades(parent, args, ctx, info) {
-      return [99, 75, 50, 49, 10];
+    posts() {
+      return [
+        {
+          id: "4333",
+          title: "Winter",
+          body: "Nice mountains with a lot of snow",
+          published: true,
+        },
+        {
+          id: "2265",
+          title: "Spring",
+          body: "Nice flavour of flowers",
+          published: false,
+        },
+      ];
     },
     me() {
       return {
