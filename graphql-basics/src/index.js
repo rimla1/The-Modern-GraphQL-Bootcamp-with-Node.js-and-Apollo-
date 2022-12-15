6fd7e054fcdc39a3c764b603aa5565207ba3d66d
Almir Muminovic <projectalmir@gmail.com>
2022-12-15 22:39:29 +0100

    Fixing type mistake

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from "graphql-yoga";
 import db from "./db";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
-import Comment from "./resolvers/Commnet";
+import Comment from "./resolvers/Comment";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
 
