2265d456b05dae95fa02350d77957959cb39aadb
Almir Muminovic <projectalmir@gmail.com>
2022-12-15 16:31:55 +0100

    Creating Post file for mutations related to the Post

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -7,18 +7,6 @@ const userService = new UserService();
 
 // Resolvers
 const resolvers = {
-  Post: {
-    author(parent, args, { db }, info) {
-      return db.users.find((user) => {
-        return user.id === parent.author;
-      });
-    },
-    comments(parent, args, { db }, info) {
-      return db.comments.filter((comment) => {
-        return comment.post === parent.id;
-      });
-    },
-  },
   User: {
     posts(parent, args, { db }, info) {
       return db.posts.filter((post) => {
