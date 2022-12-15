e14f30263dafbc6982102be8526caa9d804c483f
Almir Muminovic <projectalmir@gmail.com>
2022-12-15 16:34:14 +0100

    Creating file to resolve custom type User

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -7,22 +7,6 @@ const userService = new UserService();
 
 // Resolvers
 const resolvers = {
-  User: {
-    posts(parent, args, { db }, info) {
-      return db.posts.filter((post) => {
-        return post.author === parent.id;
-      });
-    },
-    comments(parent, args, { db }, info) {
-      return db.comments.filter((comment) => {
-        return comment.author === parent.id;
-      });
-    },
-    premium(parent, args, { db }, info) {
-      return { id: "1", isPremium: true };
-    },
-  },
-
   Comment: {
     author(parent, ags, { db }, info) {
       // data loaderi
---
+++ graphql-basics/src/resolvers/User.js
@@ -0,0 +1,17 @@
+const User = {
+  posts(parent, args, { db }, info) {
+    return db.posts.filter((post) => {
+      return post.author === parent.id;
+    });
+  },
+  comments(parent, args, { db }, info) {
+    return db.comments.filter((comment) => {
+      return comment.author === parent.id;
+    });
+  },
+  premium(parent, args, { db }, info) {
+    return { id: "1", isPremium: true };
+  },
+};
+
+export { User as default };
