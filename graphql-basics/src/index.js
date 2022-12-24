50b65b26edae95502da14f97e6bd4834e192a4ca
Almir Muminovic <projectalmir@gmail.com>
2022-12-24 21:02:01 +0100

    Exporting resolver for subscription to server

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,12 +5,14 @@ import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
+import Subscription from "./resolvers/Subscription";
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment,
