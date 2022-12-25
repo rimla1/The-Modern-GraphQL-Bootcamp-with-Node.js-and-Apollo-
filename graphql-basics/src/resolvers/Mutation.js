c7f6dc07da76b70c439117527ff745da67bdab71
Almir Muminovic <projectalmir@gmail.com>
2022-12-25 15:44:39 +0100

    Setting up publish in mutation to watch for every new comment on certain post

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -110,7 +110,7 @@ const Mutation = {
 
     return post;
   },
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
     const postExists = db.posts.some((post) => {
       return post.id === args.data.post && post.published === true;
@@ -127,6 +127,7 @@ const Mutation = {
     };
 
     db.comments.push(comment);
+    pubsub.publish(`Comment ${postExists.id}`);
     return comment;
   },
   deleteComment(parent, args, { db }, info) {
