45575a26497d7c815097467e92b12127bb18566c
Almir Muminovic <projectalmir@gmail.com>
2022-12-26 15:37:37 +0100

    Deleting count example from final project

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,18 +1,4 @@
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0;
-
-      setInterval(() => {
-        count++;
-        pubsub.publish("counter", {
-          count: count,
-        });
-      }, 1000);
-
-      return pubsub.asyncIterator("counter"); // like a chatroom name
-    },
-  },
   comment: {
     subscribe(parent, { postId }, { db, pubsub }, info) {
       const postExist = db.posts.find(
